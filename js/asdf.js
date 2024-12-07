e8017ef7cce8fa97ead070b2f8e11630243e5103
Dezső Tamás <tadezso@yettel.hu>
2024-12-07 10:47:08 +0100

    Update seqdiag-js

--- js/asdf.js
+++ js/asdf.js
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2024 Tamas Dezso (dezso.t.tamas@gmail.com)
+Copyright (c) 2024 Tamas Dezso (asdf.hu)
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the
