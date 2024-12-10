375d9eb014317c6530cb706583c0db621fbf8c18
Dezső Tamás <tadezso@yettel.hu>
2024-12-11 00:50:04 +0100

    Rename repo for better fit with github pages

--- js/asdf.js
+++ js/asdf.js
@@ -1,6 +1,6 @@
 /*
 Copyright (c) 2024 Tamás Dezső (asdf.hu)
-https://github.com/xsnpdngv/ASDF-V
+https://github.com/xsnpdngv/asdfv
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the
