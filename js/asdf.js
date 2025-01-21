8f4b2cf1dd6c49193fc339f2aa8b20b85ad911ff
Dezső Tamás <tadezso@yettel.hu>
2025-01-21 17:15:05 +0100

    Fix actor order refresh

--- js/asdf.js
+++ js/asdf.js
@@ -419,7 +419,7 @@ class AsdfModel {
             if (this.#actorOrder.length() > 0 &&
                  ! this.#arraysHaveSameElements(this.#actorOrder.array, this.diag.actors.map(element => element.name))) {
                 this.#actorOrder.clear();
-                this.#diagSrcToParse = this.#diagSrc;
+                this.#diagSrcToParse = this.#diagSrc.value;
                 this.diag = Diagram.parse(this.#diagSrcToParse);
             }
             this.#postProc();
